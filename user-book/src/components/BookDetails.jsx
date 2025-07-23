66696bc5d62bac0b24b8875b0b8448674def551b
Happy <154253997+Happyparasar@users.noreply.github.com>
2025-07-23 12:29:16 +0530

    Revert BookDetails.jsx to use localhost URLs

--- user-book/src/components/BookDetails.jsx
+++ user-book/src/components/BookDetails.jsx
@@ -2,60 +2,71 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+
 function BookDetail() {
     let param = useParams();
     let [book, setBook] = useState({});
-    let[status, setStatus]=useState("")
-    let[pincode , setPincode]=useState();
+    let [status, setStatus] = useState("");
+    let [pincode, setPincode] = useState();
+
     useEffect(() => {
         axios({
-            url: 'https://mern-bookstore-hd67.onrender.com/user/book/' + param.id,
+            url: 'http://localhost:3001/user/book/' + param.id,
             method: 'get'
         }).then((res) => {
-            console.log(res);
-            setBook(res.data.data)
-        }).catch((err) => [
-            console.log(err.message)
-        ])
-    }, [param])
+            setBook(res.data.data);
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }, [param]);
 
-    function checkStatus(){
-        let data= {
-            pincode :pincode
-        }
+    function checkStatus() {
+        let data = {
+            pincode: pincode
+        };
         axios({
-            url:"https://mern-bookstore-hd67.onrender.com/check/status",
-            method :"post",
-            data :data,
-        }).then((res)=>{
-            if(res.data.success){
-                setStatus(res.data.data)
+            url: "http://localhost:3001/check/status",
+            method: "post",
+            data: data,
+        }).then((res) => {
+            if (res.data.success) {
+                setStatus(res.data.data);
             }
-        }).catch((err)=>{
-            console.log(err.message)
-        })
+        }).catch((err) => {
+            console.log(err.message);
+        });
     }
+
     function gotopurchase() {
-        let token = localStorage.getItem('token')
-        if(token){
-            alert("You are most welcome and may proceed for payment")
-        }else{
-            alert("Please , Login First.....")
+        let token = localStorage.getItem('token');
+        if (token) {
+            alert("You are most welcome and may proceed for payment");
+        } else {
+            alert("Please, Login First...");
         }
     }
+
     return (
         <Container fluid>
             <div style={{ display: "flex", marginTop: "10px" }}>
                 <div style={{ border: '1px solid lightgrey' }}>
                     <img src={book.bookImage} height="400px" width="350px" alt="" />
-
                 </div>
-                <div style={{ marginLeft: '20px' }} >
+                <div style={{ marginLeft: '20px' }}>
                     <p style={{ fontSize: '25px', color: 'grey' }}>{book.bookName}</p>
-                    <div style={{ width: '70px', backgroundColor: 'seaGreen', borderRadius: '5px', color: 'white', padding: 'left', fontSize: '15px' }}>4.4*</div>
-                    <span style={{ fontWeight: 'bold' }}>&#x20b9;{book.finalPrice}</span><span style={{ marginLeft: '10px', color: 'grey', fontSize: '17px' }}> &#x20b9;<s>{book.price}</s></span>
+                    <div style={{
+                        width: '70px',
+                        backgroundColor: 'seaGreen',
+                        borderRadius: '5px',
+                        color: 'white',
+                        fontSize: '15px'
+                    }}>4.4*</div>
+                    <span style={{ fontWeight: 'bold' }}>&#x20b9;{book.finalPrice}</span>
+                    <span style={{ marginLeft: '10px', color: 'grey', fontSize: '17px' }}>
+                        &#x20b9;<s>{book.price}</s>
+                    </span>
                     <p style={{ fontSize: '15px', color: 'grey' }}>Author : {book.authorName}</p>
-                    <p style={{ fontSize: '15px', color: 'grey' }}>Highlights : </p>
+                    <p style={{ fontSize: '15px', color: 'grey' }}>Highlights :</p>
 
                     <li style={{ fontSize: '16px', color: 'gray' }}>Description: {book.bookDescription}</li>
                     <li style={{ fontSize: '16px', color: 'gray', marginTop: '10px' }}>Language: {book.language}</li>
@@ -63,18 +74,21 @@ function BookDetail() {
                     <li style={{ fontSize: '16px', color: 'gray', marginTop: '10px' }}>Binding: {book.binding}</li>
                     <li style={{ fontSize: '16px', color: 'gray', marginTop: '10px' }}>Publisher: {book.publisher}</li>
                     <li style={{ fontSize: '16px', color: 'gray', marginTop: '10px' }}>Seller: {book.seller}</li>
+
                     <div>
                         <label className="m-3"><h5>Check Delivery Status : </h5></label>
-                        <input type="Number" onChange={(e)=>setPincode(e.target.value)} />
-                        <Button  className="m-3 btn-dark btn-sm color-dark" onClick={checkStatus}>Check</Button>
-                        <h6 style={{color:"red"}}>{status}</h6>
+                        <input type="Number" onChange={(e) => setPincode(e.target.value)} />
+                        <Button className="m-3 btn-dark btn-sm" onClick={checkStatus}>Check</Button>
+                        <h6 style={{ color: "red" }}>{status}</h6>
                     </div>
+
                     <p>
-                        <Button varient='success' onClick={() => gotopurchase()}>Buy Now</Button>
+                        <Button variant='success' onClick={gotopurchase}>Buy Now</Button>
                     </p>
                 </div>
             </div>
         </Container>
-    )
+    );
 }
+
 export default BookDetail;
